7f448e39834b11f17acfaf5718f7918b6f930007
wangheng <wangheng@vchangyi.com>
2021-01-15 19:51:31 +0800

    '更新sequelize'

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const parser = require("koa-bodyparser");
 const catchError = require("./middlewares/exception");
 const InitManager = require("./core/init");
 
+require("./app/models/user")
+
 const app = new Koa();
 app.use(parser());
 app.use(catchError);
--- config/config.js
+++ config/config.js
@@ -1,3 +1,10 @@
 module.exports = { 
-  environment: "dev"
+  environment: "dev",
+  database: {
+    dbName: "7yue",
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root"
+  }
 }
