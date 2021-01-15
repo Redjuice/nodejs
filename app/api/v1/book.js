5ad240487f9c32311733a8f814d972f67c012d4c
wangheng <wangheng@vchangyi.com>
2021-01-15 11:24:33 +0800

    '封装全局错误'

--- app/api/v1/book.js
+++ app/api/v1/book.js
@@ -1,12 +1,12 @@
 const Router = require("koa-router");
 const router = new Router();
+const { HttpException } = require("../../../core/http-exception");
 
 router.post("/v1/:id/classic/book/latest", (ctx, next) => {
-  const path = ctx.params;
-  const query = ctx.request.query;
-  const headers = ctx.request.headers;
-  const body = ctx.request.body;
-  ctx.body = { key: "book" };
+  if (true) {
+    const error = new HttpException("错误原因", 10001, 400);
+    throw error;
+  }
 });
 
 module.exports = router;
